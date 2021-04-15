118aad69a0c4681d33ece9353575bd66e1c8695f
Josh Archer <jarcher@greenriver.edu>
2021-04-15 14:01:44 -0700

    Added an example of simple paths, view variables and view templates in a Spring project.

---
+++ cooking_tips_mvc/src/main/java/edu/greenriver/sdev/cookingtipsmvc/CookingTipsMvcApplication.java
@@ -0,0 +1,15 @@
+package edu.greenriver.sdev.cookingtipsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingTipsMvcApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CookingTipsMvcApplication.class, args);
+    }
+
+}
---
+++ cooking_tips_mvc/src/main/java/edu/greenriver/sdev/cookingtipsmvc/controllers/SteamingController.java
@@ -0,0 +1,5 @@
+package edu.greenriver.sdev.cookingtipsmvc.controllers;
+
+public class SteamingController
+{
+}
