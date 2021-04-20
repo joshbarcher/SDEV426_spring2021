9ca37ac61cc7673663f5bc09e188b7ddf2ac8569
Josh Archer <jarcher@greenriver.edu>
2021-04-20 14:13:11 -0700

    Added another example of using Thymeleaf.

---
+++ thymeleaf_practice/src/main/java/edu/greenriver/it/thymeleafdemo/controllers/ProductController.java
@@ -0,0 +1,53 @@
+package edu.greenriver.it.thymeleafdemo.controllers;
+
+import edu.greenriver.it.thymeleafdemo.model.Product;
+import edu.greenriver.it.thymeleafdemo.model.Sale;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("products")
+public class ProductController
+{
+    private Product[] fakeProducts = {
+        new Product(
+            "fishing pole",
+            79.99,
+            "A must have for any angler.",
+            Sale.ON_SALE
+        ),
+        new Product(
+            "computer monitor",
+            199.99,
+            "An LCD panel with 60hz.",
+            Sale.ONE_TIME_ONLY
+        ),
+        new Product(
+            "Refrigerator magnets",
+            1.99,
+            "A 5 pack of magnets for the fridge.",
+            Sale.NO_SALE
+        )
+    };
+
+    //URL should be http://localhost:8080/products/current
+    @RequestMapping("current")
+    public String showCurrentDeals(Model model)
+    {
+        //view variables...
+        model.addAttribute("products", fakeProducts);
+        model.addAttribute("highlighted", fakeProducts[0]);
+
+        return "current_deals";
+    }
+}
+
+
+
+
+
+
+
+
+
