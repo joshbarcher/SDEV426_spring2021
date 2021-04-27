36f12e52a24fbed9f91c742af3efe39805f33e3c
Josh Archer <jarcher@greenriver.edu>
2021-04-27 13:49:49 -0700

    Added an example of URL syntax with Thymeleaf.

--- thymeleaf_practice/src/main/java/edu/greenriver/it/thymeleafdemo/controllers/ProductController.java
+++ thymeleaf_practice/src/main/java/edu/greenriver/it/thymeleafdemo/controllers/ProductController.java
@@ -4,6 +4,7 @@ import edu.greenriver.it.thymeleafdemo.model.Product;
 import edu.greenriver.it.thymeleafdemo.model.Sale;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,24 +13,47 @@ public class ProductController
 {
     private Product[] fakeProducts = {
         new Product(
-            "fishing pole",
+            "fishing_pole",
             79.99,
             "A must have for any angler.",
             Sale.ON_SALE
         ),
         new Product(
-            "computer monitor",
+            "computer_monitor",
             199.99,
             "An LCD panel with 60hz.",
             Sale.ONE_TIME_ONLY
         ),
         new Product(
-            "Refrigerator magnets",
+            "refrigerator_magnets",
             1.99,
             "A 5 pack of magnets for the fridge.",
             Sale.NO_SALE
         )
     };
+
+    //URL should be http://localhost:8080/products/individual/fishingpole
+    @RequestMapping("individual/{name}")
+    public String showIndividualProduct(@PathVariable String name, Model model)
+    {
+        Product found = getProductByName(name);
+        model.addAttribute("product", found);
+        return "product_by_name";
+    }
+
+    private Product getProductByName(String name)
+    {
+        Product found = null;
+
+        for (Product candidate : fakeProducts)
+        {
+            if (candidate.getName().equals(name))
+            {
+                return candidate;
+            }
+        }
+        return null;
+    }
 
     //URL should be http://localhost:8080/products/current
     @RequestMapping("current")
